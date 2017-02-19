fd8f0276a9f7438ac0b8a13908d700844704258e
Joshua Bresette <jbresett@asu.edu>
2017-02-18 18:59:30 -0700

    Original Code

---
+++ FourRowSolitaire/src/FourRowSolitaire/Column.java
@@ -0,0 +1,48 @@
+
+package FourRowSolitaire;
+
+/**
+ * Class: Column
+ *
+ * Description: The Column class manages a single column on the board.
+ *
+ */
+public class Column extends CardStack
+{
+    public Column() {}
+
+    public Card push(Card card)
+    {
+        if(isEmpty() && card.getNumber() == Card.KING)
+        {
+            super.push(card);
+            return card;
+        }
+        else if(card.getColor() != peek().getColor() && card.getNumber() == peek().getNumber() - 1)
+        {
+            super.push(card);
+            return card;
+        }
+
+        return null;
+    }
+
+    public boolean isValidMove(Card card)
+    {
+        if(isEmpty() && card.getNumber() == Card.KING)
+        {
+            return true;
+        }
+        else if(!isEmpty() && card.getColor() != peek().getColor() && card.getNumber() == (peek().getNumber() - 1))
+        {
+            return false;
+        }
+
+        return false;
+    }
+
+    public boolean isValidMove(CardStack stack)
+    {
+        return isValidMove(stack.peek());
+    }
+}
